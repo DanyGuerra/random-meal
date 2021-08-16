80cada4b37dffa3d03f1ce8dfb0a43b4dd893c9d
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-08-16 12:44:28 -0500

    Comidas random con sus detalles

--- index.js
+++ index.js
@@ -14,18 +14,26 @@ const showMeal = (meal) => {
   const ingredientes = document.querySelector(".lista-ingredientes");
   const procedimiento = document.querySelector(".procedimiento");
   const tituloIngredientes = document.querySelector('.ingredients-title');
+  const detallesComida = document.querySelector('.meal-details');
+
+  detallesComida.innerHTML = ``;
+
+  if(meal.strCategory)detallesComida.innerHTML += `<p> <span class = "details-title"> Category: <span> ${meal.strCategory} </p>`;
+  if(meal.strArea)detallesComida.innerHTML += `<p> <span class = "details-title"> Area: </span> ${meal.strArea} </p>`;
+  if(meal.strTags)detallesComida.innerHTML += `<p> <span class = "details-title"> Tags: </span> ${meal.strTags} </p>`;
+  if(meal.strYoutube)detallesComida.innerHTML += `<p> <a class = "details-title" href="${meal.strYoutube}" target="_blank"> Tutorial </a> </p>`;
+
 
   tituloIngredientes.innerHTML = `Ingredients`
   mealTitle.innerHTML = `${meal.strMeal}`;
   recetaImg.innerHTML = `<img src="${meal.strMealThumb}" alt="photo">`;
 
+
   const ingredientesArray = [];
   for(let i=0; i<20; i++){
     let ingrediente = meal[`strIngredient${i}`];
     let cantidad = meal[`strMeasure${i}`];
-    if(ingrediente){
-      ingredientesArray.push(`${ingrediente} - ${cantidad}`);
-    }
+    if(ingrediente)ingredientesArray.push(`${ingrediente} - ${cantidad}`);
   }
 
   ingredientes.innerHTML = ``
